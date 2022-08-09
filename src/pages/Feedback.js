9da2110710dc6ad98e657f9fb5e3ae1562ceb7b8
Bruno Pessoa <brunofernpessoa@gmail.com>
2022-08-09 19:43:34 -0300

    Requisito 11

---
+++ src/pages/Feedback.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Feedback extends React.Component {
+  render() {
+    return (
+      <div data-testid="feedback-text">
+        feedback
+      </div>
+    );
+  }
+}
+
+export default Feedback;
